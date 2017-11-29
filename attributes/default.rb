b155da8bc5cec873841daccf77d0bf9beb3b4d06
Garry Lachman <garry@lachman.co>
2017-11-29 13:57:32 +0200

    recepie + attrs for tests

---
+++ attributes/default.rb
@@ -0,0 +1,3 @@
+default['config1']['sec1']['garry'] = 'lachman'
+default['config1']['sec1']['lachman'] = 'garry'
+default['config1']['sec2']['lachman1'] = '1garry'
--- recipes/default.rb
+++ recipes/default.rb
@@ -1,4 +1,16 @@
-ini_file 'ini' do
-  file_path '~/1.txt'
+ini_file 'ini create' do
+  file_path '/tmp/4.ini'
+  file_content node.default['config1']
   action :create
+end
+
+edit_content = {
+  'sec1' => { 'garry' => 'edited' },
+  'sec2' => { 'adding' => 'new' }
+}
+
+ini_file 'ini edit' do
+  file_path '/tmp/4.ini'
+  file_content edit_content
+  action :edit
 end
