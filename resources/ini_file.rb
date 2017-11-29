0fb7e0997eb9f975b3f3c0793d6bb1f8e7f168fd
Garry Lachman <garry@lachman.co>
2017-11-29 13:57:11 +0200

    ini_file custom resource

--- resources/ini_file.rb
+++ resources/ini_file.rb
@@ -1,9 +1,17 @@
 resource_name :ini_file
 
 property :file_path, String
+property :file_content, Hash, {}
 
 action :create do
-  file "#{new_resource.file_path}" do
-    content "file content"
-  end
+  require 'inifile'
+  ini_file = IniFile.new(:filename => new_resource.file_path, :content => new_resource.file_content)
+  ini_file.save
+end
+
+action :edit do
+  require 'inifile'
+  ini_file = IniFile.load(new_resource.file_path)
+  ini_file = ini_file.merge(new_resource.file_content)
+  ini_file.save
 end
