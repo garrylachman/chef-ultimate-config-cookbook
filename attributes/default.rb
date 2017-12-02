586267acd44953a9de940c7ce8cf3d969d9b016b
Garry Lachman <garry@lachman.co>
2017-12-02 15:52:12 +0200

    one level config for java properties

--- attributes/default.rb
+++ attributes/default.rb
@@ -26,3 +26,6 @@
 default['config1']['sec1']['garry'] = 'lachman'
 default['config1']['sec1']['lachman'] = 'garry'
 default['config1']['sec2']['lachman1'] = '1garry'
+
+default['config2']['garry'] = 'lachman'
+default['config2']['lachman'] = 'garry'
